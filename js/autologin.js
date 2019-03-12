24024a75783c49f5363e993e8cb68bce416b8d25
austin jerich <austinjrch8@gmail.com>
2019-03-12 15:06:10 -0500

    got the autologin working for facebook

--- js/autologin.js
+++ js/autologin.js
@@ -1,13 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
-	document.querySelector('#auto-login').addEventListener('click', autoLogin(document.activeElement));
+	document.querySelector('#auto-login').addEventListener('click', autoLogin);
 });
 
-function autoLogin(elem) {
-	//get current url
-	chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+function autoLogin() {
+	chrome.tabs.query({'active': true, 'currentWindow': true}, function (tabs) {
 		var url = tabs[0].url;
 		//url = window.location.href;
-		//console.log(url);
+		console.log(url);
 	
 		//get values of current URL
 		var creds = localStorage.getItem(url);
@@ -19,17 +18,24 @@ function autoLogin(elem) {
 		console.log(password1);
 		//console.log(creds);
 		
-		//find username field
-		//document.getElementById("#email");
-		document.getElementById('auto-login').value;
-
-		//input username
+		//find username field and input username
+		chrome.tabs.executeScript(
+			tabs[0].id,
+			//{code: 'document.body.style.backgroundColor = "' + color + '";'});
+			{code: 'document.getElementById("email").value = "' + username + '";'}
+		);
 		
-		//find password input box
-		document.getElementById("#pass");
-		//input password
+		//find password input and put pass
+		chrome.tabs.executeScript(
+			tabs[0].id,
+			//{code: 'document.body.style.backgroundColor = "' + color + '";'});
+			{code: 'document.getElementById("pass").value = "' + password1 + '";'}
+		);
 		
-		//find submit button
-		//click submit
+		//find and click submit button
+		chrome.tabs.executeScript(
+			tabs[0].id,
+			{code: 'document.getElementById("loginbutton").click();'}
+		);
 	});
 }
