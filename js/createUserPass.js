3d9095488d027e7bd73c7a063f499c9ed6b82a87
austin jerich <austinjrch8@gmail.com>
2019-04-03 15:55:00 -0500

    hashing the master password for when user logs in

--- js/createUserPass.js
+++ js/createUserPass.js
@@ -2,6 +2,19 @@ document.addEventListener('DOMContentLoaded', function () {
 	document.querySelector('#submit').addEventListener('click', createUserPass);
 });
 
+String.prototype.hashCode = function() {
+    var hash = 0;
+    if (this.length == 0) {
+        return hash;
+    }
+    for (var i = 0; i < this.length; i++) {
+        var char = this.charCodeAt(i);
+        hash = ((hash<<5)-hash)+char;
+        hash = hash & hash; // Convert to 32bit integer
+    }
+    return hash;
+}
+
 function createUserPass() {
 	var username = document.getElementById("username").value;
 	var pass = document.getElementById("pass1").value;
@@ -10,7 +23,8 @@ function createUserPass() {
 	if(pass == confirmPass){
 		//i can hash this instead of encrypting it
 		//var encryptedPass = CryptoJS.AES.encrypt(pass, "12345");
-		localStorage.setItem(username, [pass]);
+		hashedPass = pass.hashCode();
+		localStorage.setItem(username, [hashedPass]);
 	}
 	else{
 		console.log("passwords do not match, please try again (^:");
--- js/login.js
+++ js/login.js
@@ -2,17 +2,32 @@ document.addEventListener('DOMContentLoaded', function () {
 	document.querySelector('#loginSubmit').addEventListener('click', signin);
 });
 
+String.prototype.hashCode = function() {
+    var hash = 0;
+    if (this.length == 0) {
+        return hash;
+    }
+    for (var i = 0; i < this.length; i++) {
+        var char = this.charCodeAt(i);
+        hash = ((hash<<5)-hash)+char;
+        hash = hash & hash; // Convert to 32bit integer
+    }
+    return hash;
+}
+
 function signin(){
 	console.log("clicked login");
 	var inputedUsername = document.getElementById("username").value;
 	var inputedPassword = document.getElementById("password").value;
+	
+	hashedPasswordFromInput = inputedPassword.hashCode();
 	
 	var loggedPassword = localStorage.getItem(inputedUsername);
 	//TODO: will need to hash inputedPassword here
 	//then check against stored hash (loggedPassword)
 	
 
-	if(inputedPassword == loggedPassword){
+	if(hashedPasswordFromInput == loggedPassword){
 		console.log("success!");
 		
 		chrome.runtime.sendMessage({
